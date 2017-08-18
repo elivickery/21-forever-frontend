9d7a95040e25ba7a2383fef954708ea709a626a7
Eli <eli@Elis-MacBook-Air.local>
2017-08-18 16:14:27 -0700

    Initial commit - React Native setup

---
+++ android/app/src/main/java/com/make_it_happen_frontend/MainActivity.java
@@ -0,0 +1,15 @@
+package com.make_it_happen_frontend;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "make_it_happen_frontend";
+    }
+}
