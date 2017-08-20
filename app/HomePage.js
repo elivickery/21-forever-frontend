fbd4ae662bb85d5dd78fe0e4e2566cdcb37e9d54
Eli <eli@Elis-MacBook-Air.local>
2017-08-20 12:55:29 -0700

    Set up router to go to login and register

--- app/HomePage.js
+++ app/HomePage.js
@@ -6,40 +6,39 @@ import {
   Button
 } from 'react-native';
 import {
-  StackNavigator,
-} from 'react-navigation';
-// import make_it_happen_frontend from '../'
+  Router,
+  Scene
+} from 'react-native-router-flux';
 
 import Login from './containers/Login';
 import Register from './containers/Register';
 import User from './containers/User';
 import Main from './containers/Main';
 
-const App = () => (
-  <Router>
-    <Scene key="root">
-      <Scene key="login" component={Login} title="Login"/>
-      <Scene key="register" component={Register} title="Register"/>
-      <Scene key="home" component={Home}/>
-    </Scene>
-  </Router>
-);
-
 export default class make_it_happen_frontend extends Component {
 
-  static navigationOptions = {
-      title: 'Welcome',
-    };
-
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Make It Happen
-        </Text>
-        <Button title="Log In" onPress={this.></Button>
 
-      </View>
+    return (
+      <Router>
+        <Scene key="root">
+        <Scene key="main"
+          component={Main}
+          title="Make It Happen"
+          initial
+        />
+        <Scene
+          key="login"
+          component={Login}
+          title="Login"
+        />
+        <Scene
+          key="register"
+          component={Register}
+          title="Register"
+        />
+      </Scene>
+    </Router>
     );
   }
 }
