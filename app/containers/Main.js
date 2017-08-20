7247b6c68f2018172af6e90a42d20a99a13ec63c
Elsha Kwee <elsha.e.k@gmail.com>
2017-08-20 11:21:31 -0700

    Rename sessions folder to containers folder

---
+++ app/containers/Main.js
@@ -0,0 +1,98 @@
+// This code is taken from https://github.com/itzikbenh/React-Native-on-Rails
+
+'use strict';
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  TouchableHighlight,
+  AsyncStorage,
+  Text,
+  View
+} from 'react-native';
+import { Actions } from 'react-native-router-flux';
+
+
+const ACCESS_TOKEN = 'access_token';
+
+class Root extends Component {
+
+  componentWillMount() {
+    this.getToken();
+  }
+
+  async getToken() {
+    try {
+      let accessToken = await AsyncStorage.getItem(ACCESS_TOKEN);
+      if(!accessToken) {
+          console.log("Token not set");
+      } else {
+          this.verifyToken(accessToken)
+      }
+    } catch(error) {
+        console.log("Something went wrong");
+    }
+  }
+  //If token is verified we will redirect the user to the home page
+  async verifyToken(token) {
+    let accessToken = token
+
+    try {
+      let response = await fetch('https://afternoon-beyond-22141.herokuapp.com/api/verify?session%5Baccess_token%5D='+accessToken);
+      let res = await response.text();
+      if (response.status >= 200 && response.status < 300) {
+        //Verified token means user is logged in so we redirect him to home.
+        this.navigate('home');
+      } else {
+          //Handle error
+          let error = res;
+          throw error;
+      }
+    } catch(error) {
+        console.log("error response: " + error);
+    }
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>Welcome Friend </Text>
+        <TouchableHighlight onPress={() => Actions.login()} style={styles.button}>
+          <Text style={styles.buttonText}>Register</Text>
+        </TouchableHighlight>
+        <TouchableHighlight onPress={() => Actions.register()} style={styles.button}>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableHighlight>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+    padding: 10,
+    paddingTop: 180
+  },
+  button: {
+    height: 50,
+    backgroundColor: '#48BBEC',
+    alignSelf: 'stretch',
+    alignItems: 'center',
+    marginTop: 10,
+    justifyContent: 'center'
+  },
+  buttonText: {
+    fontSize: 22,
+    color: '#FFF',
+    alignSelf: 'center'
+  },
+  title: {
+    fontSize: 25,
+    marginBottom: 15
+  }
+});
+
+
+export default Root
