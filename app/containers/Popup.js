83d9c47f1b8ae80f941c772e4e365505d9349653
Elsha Kwee <elsha.e.k@gmail.com>
2017-08-20 23:30:23 -0700

    ADD popup container component

---
+++ app/containers/Popup.js
@@ -0,0 +1,27 @@
+import React, { PropTypes, Component } from 'react'
+import { View, Image } from 'react-native'
+
+export default class Popup extends Component {
+  static propTypes = {}
+  state = {}
+  render () {
+    return (
+      <View>
+        <Image
+          style={styles.container}
+          source={{uri: 'https://facebook.github.io/react/img/logo_og.png'}}
+        />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 100,
+    height: 100
+  }
+});
