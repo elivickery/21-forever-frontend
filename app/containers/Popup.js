956e877396d07c028a9976a5a551303f0d7697cd
Elsha E. Kwee <elsha.e.k@gmail.com>
2017-08-21 00:50:45 -0700

    Update popup.js
    Add StyleSheet function

--- app/containers/Popup.js
+++ app/containers/Popup.js
@@ -1,5 +1,5 @@
 import React, { PropTypes, Component } from 'react'
-import { View, Image } from 'react-native'
+import { View, Image, StyleSheet } from 'react-native'
 
 export default class Popup extends Component {
   static propTypes = {}
