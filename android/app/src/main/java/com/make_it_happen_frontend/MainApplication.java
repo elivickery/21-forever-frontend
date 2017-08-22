c0bbd194424f950e64f837ed77bce2fcc6b5ec8c
Joe Alfaro <joealfaro@Joes-MacBook-Air.local>
2017-08-22 16:57:45 -0700

    Trying to make Android compile. Still not working

--- android/app/src/main/java/com/make_it_happen_frontend/MainApplication.java
+++ android/app/src/main/java/com/make_it_happen_frontend/MainApplication.java
@@ -3,6 +3,8 @@ package com.make_it_happen_frontend;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.airbnb.android.react.maps.MapsPackage;
+import com.airbnb.android.react.lottie.LottiePackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +26,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new MapsPackage(),
+            new LottiePackage(),
             new VectorIconsPackage()
       );
     }
