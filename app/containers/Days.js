15204352f42a093067b9c569c5a18d979f17e05b
Eli <eli@Elis-MacBook-Air.local>
2017-08-23 11:52:41 -0700

    Add componentwillmount function to get current state

--- app/containers/Days.js
+++ app/containers/Days.js
@@ -16,6 +16,35 @@ export default class Days extends Component {
     this.buttonPressInComplete = this.buttonPressInComplete.bind(this)
   }
 
+  componentWillMount() {
+
+    axios.get("https://make-it-happen-api.herokuapp.com/api/days/current", {
+      params: {
+        access_token: this.props.accessToken
+      }
+    })
+    .then((response)=> {
+      console.log(response.data)
+
+      if(response.data.complete == 'achieved') {
+        this.setState({
+          completed: true,
+          failed: false
+        })
+      } else if(response.data.complete == 'failed') {
+        this.setState({
+          completed: false,
+          failed: true
+        })
+      }
+
+    })
+    .catch(function (error) {
+
+    });
+
+  }
+
   //Sends and changes status. Check heroku logs.
   buttonPressComplete(){
     axios.put('https://make-it-happen-api.herokuapp.com/api/days/edit', {
