f20d734199aa9a57e609f7aa963b2b13b164a3e0
Eli <eli@Elis-MacBook-Air.local>
2017-08-25 08:35:25 -0700

    Temporarily disable yellow debug errors for demo

--- index.ios.js
+++ index.ios.js
@@ -1,3 +1,6 @@
+// Temporarily disable yellow error messages for demo env
+console.disableYellowBox = true;
+
 import make_it_happen_frontend from './app/HomePage.js';
 import { AppRegistry } from 'react-native';
 
